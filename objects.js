d439beba65b275140bf99b0d0e1644855727480d
Nick Casciola <s0948644@monmouth.edu>
2017-10-16 18:08:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,6 @@ function updatePlaylist(playlist, artistName, songTitle)  {
 };
 
 function removeFromPlaylist(playlist, artistName) {
-  return playlist = delete playlist.artistName;
+  delete playlist.artistName;
+  playlist;
 };
