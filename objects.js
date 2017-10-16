7dedda3793921b4ba2a8343ea4fe6427a823b623
Nick Casciola <s0948644@monmouth.edu>
2017-10-16 18:00:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,9 @@ var playlist = {Drake: 'Forever'};
 
 function updatePlaylist(playlist, artistName, songTitle)  {
   return Object.assign({}, playlist, {artistName: songTitle});
-}
+};
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
   return playlist;
-}
+};
